ddd8653cdff7ec43a510c0d137fbc27913d6bf44
shreyash bobalade <shreyashbobalade2006@gmail.com>
2026-04-04 12:17:41 +0530

    new day new trial

--- script.js
+++ script.js
@@ -1,37 +1,5 @@
-// Typing Effect
-const roles = ["Web Developer", "Programmer", "Problem Solver"];
-let i = 0;
-let j = 0;
-let current = "";
-let isDeleting = false;
-
-function type() {
-  current = roles[i];
-
-  if (!isDeleting) {
-    document.getElementById("typing").textContent =
-      current.substring(0, j++);
-  } else {
-    document.getElementById("typing").textContent =
-      current.substring(0, j--);
-  }
-
-  if (j === current.length) isDeleting = true;
-  if (j === 0) {
-    isDeleting = false;
-    i = (i + 1) % roles.length;
-  }
-
-  setTimeout(type, isDeleting ? 50 : 100);
-}
-
-type();
-
-// Smooth Scroll
-document.querySelectorAll("a").forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
   });
-});
+}
